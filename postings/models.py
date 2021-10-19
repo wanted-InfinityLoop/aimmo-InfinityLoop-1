71f37c5526984175304b77497f9bce2eebebeafa
YeJun <hayejun1013@naver.com>
2021-10-20 00:09:30 +0900

    feat : add posting modeling scripts

--- postings/models.py
+++ postings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    title = models.CharField(max_length=32, null=True)
+    text = models.CharField(max_length=64, unique=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "postings"
