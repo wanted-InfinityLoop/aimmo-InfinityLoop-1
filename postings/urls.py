0f45340949658e21d45e8acbaf47dcb4793ed266
YeJun <hayejun1013@naver.com>
2021-10-20 14:31:30 +0900

    feat: add update posting method

--- postings/urls.py
+++ postings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/list", views.PostingListView.as_view()),
-    path("/<int:posting_id>", views.PostingView.as_view()),
     path("", views.PostingView.as_view()),
+    path("/<int:posting_id>", views.PostingView.as_view()),
+    path("/list", views.PostingListView.as_view()),
 ]
--- postings/views.py
+++ postings/views.py
@@ -8,36 +8,6 @@ from .models import Posting
 from core.utils import login_decorator
 
 
-class PostingListView(View):
-    def get(self, request):
-        try:
-            OFFSET = int(request.GET.get("offset", 0))
-            LIMIT = int(request.GET.get("limit", 10))
-
-            postings = Posting.objects.all().order_by("-created_time")[
-                OFFSET : OFFSET + LIMIT
-            ]
-
-            result = {
-                "count": postings.aggregate(count=Count("id")).get("count", 0),
-                "postings": [
-                    {
-                        "id": posting.id,
-                        "author": posting.author.name,
-                        "title": posting.title,
-                        "text": posting.text,
-                        "created_time": posting.created_time,
-                        "updated_time": posting.updated_at,
-                    }
-                    for posting in postings
-                ],
-            }
-
-            return JsonResponse({"result": result}, status=200)
-        except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-
 class PostingView(View):
     def get(self, request, posting_id):
         if not Posting.objects.filter(id=posting_id).exists():
@@ -72,5 +42,58 @@ class PostingView(View):
             return JsonResponse(
                 {"message": f"{posting.title} has successfully posted"}, status=201
             )
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+    @login_decorator
+    def put(self, request, posting_id):
+        try:
+            if not Posting.objects.filter(id=posting_id).exists():
+                return JsonResponse(
+                    {"message": f"POSTING_{posting_id}_NOT_FOUND"}, status=404
+                )
+
+            posting = Posting.objects.get(id=posting_id)
+
+            if request.user.id != posting.author_id:
+                return JsonResponse({"message": "FORBIDDEN"}, status=403)
+
+            data = json.loads(request.body)
+            posting.text = data["text"]
+            posting.save()
+
+            return JsonResponse(
+                {"message": f"{posting.title} has successfully updated"}, status=200
+            )
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+
+class PostingListView(View):
+    def get(self, request):
+        try:
+            OFFSET = int(request.GET.get("offset", 0))
+            LIMIT = int(request.GET.get("limit", 10))
+
+            postings = Posting.objects.all().order_by("-created_time")[
+                OFFSET : OFFSET + LIMIT
+            ]
+
+            result = {
+                "count": postings.aggregate(count=Count("id")).get("count", 0),
+                "postings": [
+                    {
+                        "id": posting.id,
+                        "author": posting.author.name,
+                        "title": posting.title,
+                        "text": posting.text,
+                        "created_time": posting.created_time,
+                        "updated_time": posting.updated_at,
+                    }
+                    for posting in postings
+                ],
+            }
+
+            return JsonResponse({"result": result}, status=200)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
