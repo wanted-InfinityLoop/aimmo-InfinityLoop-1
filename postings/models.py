fd968c7b63777b6634bfddb379ea366297feab13
YeJun <hayejun1013@naver.com>
2021-10-20 10:39:24 +0900

    fix: rename model class on postings app

--- postings/models.py
+++ postings/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class User(models.Model):
-    title = models.CharField(max_length=32, null=True)
-    text = models.CharField(max_length=64, unique=True)
+class Posting(models.Model):
+    title = models.CharField(max_length=128, default="")
+    text = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey("users.User", on_delete=models.CASCADE)
--- postings/views.py
+++ postings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from .models import Posting
+from core.utils import login_decorator
+
+
+class PostingListView(View):
+    def get(self, request):
+        try:
+            OFFSET = request.GET.get("offset", 0)
+            LIMIT = request.GET.get("limit", 30)
+
+            postings = Posting.objects.all().order_by("-created_time")[
+                OFFSET : LIMIT + OFFSET
+            ]
+
+            return
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
