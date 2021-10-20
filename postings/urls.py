86ff43234a71b1935bbb1f56c0980b71fc16100e
YeJun <hayejun1013@naver.com>
2021-10-20 11:08:40 +0900

    feat: add postings list get method

--- postings/urls.py
+++ postings/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [path("", views.PostingListView.as_view())]
--- postings/views.py
+++ postings/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import JsonResponse
 from django.views import View
+from django.db.models import Count
 
 from .models import Posting
 from core.utils import login_decorator
@@ -13,10 +14,23 @@ class PostingListView(View):
             OFFSET = request.GET.get("offset", 0)
             LIMIT = request.GET.get("limit", 30)
 
-            postings = Posting.objects.all().order_by("-created_time")[
-                OFFSET : LIMIT + OFFSET
-            ]
+            postings = Posting.objects.all().order_by("-created_time")
 
-            return
+            result = {
+                "count": postings.aggregate(count=Count("id")).get("count", 0),
+                "postings": [
+                    {
+                        "id": posting.id,
+                        "author": posting.author.name,
+                        "title": posting.title,
+                        "text": posting.text,
+                        "created_time": posting.created_time,
+                        "updated_time": posting.updated_time,
+                    }
+                    for posting in postings
+                ][OFFSET : LIMIT + OFFSET],
+            }
+
+            return JsonResponse({"result": result}, status=200)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
