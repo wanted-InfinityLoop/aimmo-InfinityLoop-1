980e043d249b53f3473f73507844b0d89efaa1a9
YeJun <hayejun1013@naver.com>
2021-11-02 22:03:01 +0900

    feat: Add Comment & Sub Comment View

--- postings/urls.py
+++ postings/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.PostingCreateView.as_view()),
     path("/<int:posting_id>", views.PostingView.as_view()),
+    path("/comment/<int:posting_id>", views.CommentView.as_view()),
+    path("/sub-comment/<int:parent_comment_id>", views.SubCommentView.as_view()),
     path("/list", views.PostingListView.as_view()),
 ]
